c3b60bf2a896803bbc1681f79fd1ba62164e9934
MarkussIPA22 <125544038+MarkussIPA22@users.noreply.github.com>
2023-10-12 15:23:54 +0300

    Add files via upload

---
+++ src/poga.js
@@ -0,0 +1,13 @@
+function Poga (props){
+
+
+    return (
+        <div>
+            <p>{props.title}</p>
+            <input>
+            
+            </input>
+        </div>
+    )
+}
+export default Poga
